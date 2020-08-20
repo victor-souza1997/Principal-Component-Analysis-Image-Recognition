7e2987ac60cdf1b10dafa1a11aff38d45d8fc4f0
victor-souza1997 <victor-souza1997@hotmail.com>
2020-08-20 19:03:23 -0300

    second commit

--- Image analysis/source/images.py
+++ Image analysis/source/images.py
@@ -18,31 +18,33 @@ class Image():
         print(str(width) +" "+ str(height)+" "+str(width*height))
         M = np.fromfile(pgmf, dtype = np.uint8)
         pgmf.close()
-        return M
+        return M[0:width*height:4]
         vetorimg = read_pgm(2)
         print(vetorimg)
     def addVectorImg(self, img):
         #f=open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "a+")
-        with open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "a+") as myfile:
+        with open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "a") as myfile:
             np.savetxt(myfile,img)
         myfile.close()
     def readImage(self, n, length):
         #with open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "r") as myfile:
         return np.loadtxt("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt",delimiter="\n")[(n-1)*length:length*n]
     def showImage(self, img, length, width):
-        im = img.reshape(width, length)
+        im = img.reshape(length, width)
         plt.gray()
         plt.imshow(im)
         plt.show()
 
-#width = 112 and length = 92 in all the imagens
-n = 10
-length = 92
-width = 112
-img = Image()
-#for i in range(1,11):
-#    img.addVectorImg(img.getImgVector(i))
-#x = img.readImage(n, width*length)
-#img.showImage(x, length,width)
+#width = 92 and length = 112 in all the imagens
+#n = 10
+#width = 23
+#length = 112
+#img = Image()
+#for i in range(1,11):#
+#    M = img.getImgVector(i)
+#    print(M.shape)#
+#    img.addVectorImg(M)
+#x = img.readImage(4, width*length).T
+#img.showImage(x,  length, width)
 
 #def reScaleImage(self, M, resize):
--- Image analysis/source/pca.py
+++ Image analysis/source/pca.py
@@ -1,27 +1,66 @@
 
 from images import Image
 import numpy as np
+import time
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+#import cupy as cp
+#from skcuda import linalg
 
 #esta classe tem como objetivo de processar às imagens
 class PCA():
     def __init__(self):
         self.img = Image()
-
-    def getMatrix(self, M):   #função que retorna matrix de covariancia
-        return np.outer(M,M)
-
+        self.A = 0  #matrix variância
+        self.m = 0  #vetor média
+    def getMatrix(self, M):
+        A = np.zeros((2688,2688))
+        M = M.T
+        for i in range(0,10):   #função que retorna matrix de covariancia
+            A = A + np.outer(M[:,i],M[:,i])#np.outer(M[i,:],M[i,:]) #para a múltiplicação da matrix MxM^T eu utilizei uma função pronta de Python
+        return A
     def getEignValues(self, M): #função que retorna o valor dos autovalores da matrix
         V, D = np.linalg.eig(M)
         return np.diagonal(D)
 
     def meanVector(self, length, N):
-        F = np.array([0]*length)
+        F = np.zeros(length)
+        self.A = np.zeros((10, length))
         for n in range(1,N+1):
-            F = F+np.array(self.img.readImage(n, length))
-        return F/N
+            D = np.array(self.img.readImage(n, length))
+            F = F + D
+            self.A[n-1,:] = D
+        self.m = F/N
 
+    def getMean(self):
+        return self.m
+
+    def setVariance(self):
+        self.m = np.outer(np.ones(10),self.m)
+        self.A = self.A - self.m
+        return self.A
+
+    def getVariance(self):
+        return self.A
 
 img = PCA()
-#M = img.getMatrix(np.array([1,2,3]))
-M = (img.meanVector(112*92, 10))
-print(img.getMatrix(M))
+img.meanVector(2688, 10)
+t0 = time.time()
+img.setVariance()
+
+M = (img.getVariance())
+C = (img.getMatrix(M))
+#print(C.shape)
+#C = (np.random.rand(10000,10000))
+V, D = np.linalg.eigh(C)#linalg.eig(C)
+#V, D = la.eig(C)
+#print(V.shape, D.shape)
+#plt.plot(V)
+#plt.show()
+#vr_gpu, w_gpu = linalg.eig(C, 'N', 'V')
+#plt.plot(D)
+#plt.show()
+t1 = time.time()
+
+total = t1-t0
+print(total)
