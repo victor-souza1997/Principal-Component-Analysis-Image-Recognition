7d0105a4f4c40f4c1c7e22fcfcd0e64d64bde258
vistor-souza1997 <victor-souza1997@hotmail.com>
2022-10-25 12:54:18 -0300

    adds file to handle images

--- Image analysis/source/images.py
+++ Image analysis/source/images.py
@@ -1,14 +1,20 @@
 import numpy as np
 from PIL import Image
-from PIL import Image
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import os, sys
+import time
 
-class Image():
+class Imageprocessing():
+
+    def __init__(self, folder, files):
+        self.folder = folder
+        self.files = files
+        path = os.getcwd()
+        self.fileDir = os.path.abspath(os.path.join(path, os.pardir))
     def getImgVector(self, face_set, img):
-        ##"""Return a raster of integers from a PGM as a list of lists."""
-        path ='C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/conteudo/s'+str(face_set)+'/'+str(img)+'.pgm'
+        """Return a raster of integers from a PGM as a list of lists."""
+        path =self.fileDir+'/utils/conteudo/'+str(face_set)+'/'+str(img)+'.pgm'
         pgmf = open(path, 'rb')
         header = pgmf.readline()
         assert header[:2] == b'P5'
@@ -18,35 +24,25 @@ class Image():
         print(str(width) +" "+ str(height)+" "+str(width*height))
         M = np.fromfile(pgmf, dtype = np.uint8)
         pgmf.close()
-        return M[0:width*height:4]
         vetorimg = read_pgm(2)
         print(vetorimg)
-
+        return M[0:width*height:4]
     def addVectorImg(self, face_set,img):
         #f=open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "a+")
-        with open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face"+str(face_set)+".txt", "a") as myfile:
+        with open(self.fileDir+"/utils/imagens/"+self.folder+"/"+self.files+str(face_set)+".txt", "a") as myfile:
             np.savetxt(myfile,img)
         myfile.close()
-    def readImage(self, face_set, n, length):
-        #with open("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face1.txt", "r") as myfile:
-        return np.loadtxt("C:/Users/victo/OneDrive/Documentos/Codigos/Drone Tracker/Image analysis/utils/imagens/faces/face"+str(face_set)+".txt",delimiter="\n")[(n-1)*length:length*n]
+    def readImagevector(self, face_set, n, length):
+        return np.loadtxt(self.fileDir+"/utils/imagens/"+self.folder+"/"+self.files+str(face_set)+".txt",delimiter="\n")[(n-1)*length:length*n]
     def showImage(self, img, length, width):
         im = img.reshape(length, width)
         plt.gray()
         plt.imshow(im)
         plt.show()
 
-#width = 92 and length = 112 in all the imagens
-#n = 10
-width = 23
-length = 112
-img = Image()
-face_set = 1
-#for i in range(1,11):#
-    #M = img.getImgVector(face_set,i)
-    #print(M.shape)#
-    #img.addVectorImg(face_set, M)
-x = img.readImage(face_set, 1,  width*length).T
-img.showImage(x,  length, width)
-
-#def reScaleImage(self, M, resize):
+    def setWholeSet(self, setN):
+        for i in range(1,setN+1):
+            img = np.loadtxt(self.fileDir+"/utils/imagens/"+self.folder+"/"+self.files+str(face_set)+".txt",delimiter="\n")
+            with open(self.fileDir+"/utils/imagens/faces/face.txt", "a") as myfile:
+                np.savetxt(myfile,img)
+            myfile.close()
